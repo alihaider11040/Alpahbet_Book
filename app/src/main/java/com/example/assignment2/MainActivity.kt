7b2da66f16a30eb302f3ae95917957b5c8239031
Ali Haider <alihaider11040@gmail.com>
2022-04-03 22:23:45 +0500

    Initial structure ready

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -2,21 +2,22 @@ package com.example.assignment2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var button1 = findViewById<Button>(R.id.button1)
+        button1.setOnClickListener(View.OnClickListener
+        {
+            {
+                startActivity(Intent(this, a::class.java));
+            }
+        })
     }
-    Button button1= findViewById<view>(R.id.button1) as Button
-    button1.setOnClickListner(new View.OnClickListner()
-    {
-        @Override
-        public void onClick(View v) {
-            startActivity(new Intent(MainActivity.this, a.class));
-        }
-    })
-
 
 
 
